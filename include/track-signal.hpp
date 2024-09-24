9fefe220fb883b566ef43d047e9eb315b5a2db86
Claus Gregers Jørgensen <cgj@gaffy.dk>
2024-09-24 20:22:27 +0200

    Updated

--- include/track-signal.hpp
+++ include/track-signal.hpp
@@ -1,8 +1,12 @@
 #pragma once
 
+class TrackSignal;
+
 #include "track-object.hpp"
 #include "linked-list.hpp"
 
 class TrackSignal : public TrackObject, public linked_list<TrackSignal>
 {
+	public:
+	TrackSignal(lid_t);
 };
